795ac296715a268dabb48237e961af7220857941
uxokpro1234 <81376237+uxokpro1234@users.noreply.github.com>
2026-09-19 14:17:00 +0300

    Implement dividers finding functionality
    Added functions to find and print common dividers of two natural numbers.

--- A29/main.py
+++ A29/main.py
@@ -1,1 +1,36 @@
+def divide(i):
+    for x in range(1, i + 1):
+        if i % x == 0:
+            dividers.append(x)
 
+def findDividers():
+    for i in range(len(dividers)):
+        for j in range(i + 1, len(dividers)):
+            if dividers[i] == dividers[j]:
+                print(dividers[i], end=" ")
+                break
+
+while True:
+    try:
+        a, b = map(int, input("Enter two natural numbers: ").split())
+
+        if a <= 0 or b <= 0:
+            print("Error: numbers must be natural numbers!")
+            continue
+
+        dividers = []
+
+        divide(a)
+        divide(b)
+
+        print("Dividers for both numbers: ", end=" ")
+        findDividers()
+        print()
+
+        c = input("Do you want to continue? Y/n: ")
+
+        if c.lower() != "y":
+            break
+
+    except ValueError:
+        print("Error: incorrect data type!")
